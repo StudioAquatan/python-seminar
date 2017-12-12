0496bc57a48fcc0e410574cad57eb9522781ffb2
taxio <taxio.2029@gmail.com>
2017-12-12 19:22:04 +0900

    add type sample

---
+++ samples/ex1_types.py
@@ -0,0 +1,33 @@
+if __name__ == '__main__':
+
+    # int
+    a = 100
+    print(type(a), a)
+
+    # float
+    a = 0.1
+    print(type(a), a)
+
+    # str
+    a = 'hogehoge'
+    print(type(a), a)
+
+    # bool
+    a = True
+    print(type(a), a)
+
+    # list
+    a = [1, 2, 3, 4]
+    print(type(a), a)
+
+    # list multi
+    a = [1, '2', 3.0, [4.1, 4.2]]
+    print(type(a), a)
+
+    # tuple
+    a = (1, 2, 3, 4, 5)
+    print(type(a), a)
+
+    # dict
+    a = {'hoge': 1, 'foo': 'bar'}
+    print(type(a), a)
